e135052a0263e28b914cfc321cffdce3d5d44fa8
huanyang123 <huanwang.yang@gmail.com>
2021-06-08 08:23:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 
 setuptools.setup(
-	name = 'ML-modules', #this should be unique
+	name = 'ml', #this must be unique
 	include_package_data=True,
 	version = '0.1',
 	author = 'Huanwang (Henry) Yang',
